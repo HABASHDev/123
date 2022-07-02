39a9aea9497a2167700fd7e09943302c21ca342e
HABASH <81362411+HABASHDev@users.noreply.github.com>
2022-07-02 16:44:13 +0300

    Update guildCreate.js

--- source/events/guildCreate.js
+++ source/events/guildCreate.js
@@ -10,7 +10,7 @@ const db = require("quick.db");
 module.exports = async (client, guild) => {
   try {
     guild.channels
-      .create("ReXom 🎶", {
+      .create("𝐈𝐦𝐦𝐨𝐫𝐭𝐚𝐥𝐬 🎶", {
         type: "text",
         permissionOverwrites: [
           {
@@ -19,32 +19,31 @@ module.exports = async (client, guild) => {
             allow: ["VIEW_CHANNEL", "READ_MESSAGE_HISTORY", "ADD_REACTIONS"],
           },
         ],
-        reason: "Rexom",
+        reason: "𝐈𝐦𝐦𝐨𝐫𝐭𝐚𝐥𝐬",
         topic: "Give your server a tune. 🎶",
       })
       .then((channel) => {
         let embed = new MessageEmbed()
           .setAuthor(
-            "ReXom",
+            "Sadk",
             client.user.avatarURL({ dynamic: true }),
-            "https://discord.com/users/746696275214467112"
+            "https://discord.com/users/657596988149202997"
           )
           .setColor("BLUE")
           .setImage(
-            "https://camo.githubusercontent.com/0b6082ac62d1a2b9257aafe9e5e4e82e10efa73e07bb306a0717131e877be8bf/68747470733a2f2f6d656469612e646973636f72646170702e6e65742f6174746163686d656e74732f3834353130373434333537333731393131322f3835393232323532393933393231303235302f53637265656e73686f745f323032312d30362d32392d30322d30332d30322d36335f33613633373033376433356639356335646263646363373565363937636539312e6a7067"
+            "https://cdn.discordapp.com/icons/947196669341106306/7af1e18bb50cce6d2d4730755e08ae1e.png?size=10246e65742f6174746163686d656e74732f3834353130373434333537333731393131322f3835393232323532393933393231303235302f53637265656e73686f745f323032312d30362d32392d30322d30332d30322d36335f33613633373033376433356639356335646263646363373565363937636539312e6a7067"
           )
-          .setTitle("**ReXom github page**")
-          .setURL("https://github.com/DevelopersSupportAR/rexom.git")
+          .setTitle("**Link Server Is**")
+          .setURL("https://discord.gg/QYWmeMWu4A")
           .setFooter(
-            "Bot Made By: NIRO",
+            "The bot was made by: Sadk, nicknamed Habash",
             client.user.avatarURL({ dynamic: true })
           )
           .setTimestamp()
-          .setDescription(`**Thanks For Using \`ReXom\` Music Bot**
+          .setDescription(`**شكراً ڵـڱ لستخدام \`Sadk Bot for server imortals\` بوت الموسيقى**
             
-            > Developer: NIRO
-            > Bot Support Server: [Here](https://discord.com/users/746696275214467112)
-            > Bot Github Page: [Here](https://github.com/DevelopersSupportAR/rexom.git)`);
+            > Developer: Sadk
+            > Bot imortals Server: [Here](https://discord.com/users/657596988149202997)`);
         channel.send({
           content: "> **Give your server a tune. 🎶**",
           embeds: [embed],
