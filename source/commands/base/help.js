fa265dc29dc4690af689d62f97b127002b81f153
HABASH <81362411+HABASHDev@users.noreply.github.com>
2022-07-02 13:40:06 +0300

    Update help.js

--- source/commands/base/help.js
+++ source/commands/base/help.js
@@ -6,7 +6,7 @@ const commands = require("../../index").commands;
 module.exports = {
   name: "help",
   aliases: ["h"],
-  description: "Give's You The Commands List",
+  description: "لأضهار قائمة اوامر البوت",
 
   /**
    *
@@ -17,7 +17,7 @@ module.exports = {
   run: async (client, message, args, prefix, lang) => {
     try {
       const embed =
-        //.setImage("https://camo.githubusercontent.com/d338bbe1d594c830f1f248d4dde6aa1b8c38c7c4ae7ede289ae9caf1d8f277bd/68747470733a2f2f6d656469612e646973636f72646170702e6e65742f6174746163686d656e74732f3734333838303336333333313432303234312f3930323731313630393131323236343830342f756e6b6e6f776e2e706e67")
+        //.setImage("https://cdn.discordapp.com/icons/947196669341106306/7af1e18bb50cce6d2d4730755e08ae1e.png?size=10246e65742f6174746163686d656e74732f3734333838303336333333313432303234312f3930323731313630393131323236343830342f756e6b6e6f776e2e706e67")
         new MessageEmbed()
           .setTitle(`Help Commands`)
           .setColor(0x2f3136)
@@ -26,7 +26,7 @@ module.exports = {
             message.guild.name,
             message.guild.iconURL({ dynamic: true })
           )
-          .setDescription(`**[reXom](https://github.com/DevelopersSupportAR/rexom.git)**, reXom give you server a toune 🎶\n
+          .setDescription(`**[Տαძκ ](https://github.com/DevelopersSupportAR/rexom.git)**, reXom give you server a toune 🎶\n
             Version: \`3.1.2\`
             Prefix: ${prefix}\n
             `);
