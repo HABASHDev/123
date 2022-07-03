ec2af1d189e11c53cec7ccb37293a0a2e05b9dca
HABASH <81362411+HABASHDev@users.noreply.github.com>
2022-07-03 18:04:30 +0300

    Update devs.js

--- source/interactions/utilities/devs.js
+++ source/interactions/utilities/devs.js
@@ -3,7 +3,7 @@ const db = require('quick.db');
 
 module.exports = {
     name: "devs",
-    description: "Bot Devs",
+    description: "لأضهار مطورين البوت الاسطوريين",
     type: 'CHAT_INPUT',
 
     /**
@@ -16,22 +16,18 @@ module.exports = {
     run: async(client, interaction, args) => {
         try {
             let embed = new MessageEmbed()
-                .setAuthor('reXom!.', client.user.avatarURL({ dynamic: true }), 'https://discord.com/users/746696275214467112')
+                .setAuthor('𝐈𝐦𝐦𝐨𝐫𝐭𝐚𝐥𝐬!.', client.user.avatarURL({ dynamic: true }), 'https://discord.com/users/657596988149202997')
                 .setColor('DARK_GOLD')
                 .addFields({
                     name: "Developers name",
-                    value: "ニロ, ᴹᵃᴿˢ#3121",
+                    value: "𝐈𝐒・HΔƁΔЅH .𖤐#3300",
                     inline: true
                 }, {
-                    name: "bot github page",
-                    value: '[Here](https://github.com/DevelopersSupportAR/rexom.git)',
-                    inline: true
-                }, {
-                    name: "bot support server",
-                    value: "[Here](https://discord.com/users/746696275214467112)",
+                    name: "Server Is",
+                    value: "[Here](https://discord.com/users/657596988149202997)",
                     inline: true
                 })
-                .setImage("https://camo.githubusercontent.com/0b6082ac62d1a2b9257aafe9e5e4e82e10efa73e07bb306a0717131e877be8bf/68747470733a2f2f6d656469612e646973636f72646170702e6e65742f6174746163686d656e74732f3834353130373434333537333731393131322f3835393232323532393933393231303235302f53637265656e73686f745f323032312d30362d32392d30322d30332d30322d36335f33613633373033376433356639356335646263646363373565363937636539312e6a7067");
+                .setImage("https://cdn.discordapp.com/icons/947196669341106306/7af1e18bb50cce6d2d4730755e08ae1e.png?size=1024f3835393232323532393933393231303235302f53637265656e73686f745f323032312d30362d32392d30322d30332d30322d36335f33613633373033376433356639356335646263646363373565363937636539312e6a7067");
             interaction.followUp({ embeds: [embed], allowedMentions: {
             repliedUser: false
         }, ephemeral: false });
